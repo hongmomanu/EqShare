bee490abf4666c073c0761d4254bcecbd9d0b73b
hongmomanu <hongmomanu@gmail.com>
2014-09-26 15:05:36 +0800

    jack commit

--- app.nw/app/view/eqimmain/EarthListGrid.js
+++ app.nw/app/view/eqimmain/EarthListGrid.js
@@ -22,19 +22,19 @@ Ext.define('EqimPrj.view.eqimmain.EarthListGrid', {
 
             columns: [
                 {header: '详细信息',dataIndex: 'location',flex: 1,renderer : function(v,m,r) {
-                    console.log(r);
-                    testobj=r;
-                    var str='<ul><li>接收时间:'+r.get('stime')+'</li><li>来源:'+ r.get('cname')
+                    //console.log(r);
+                    //testobj=r;
+                    var str='<ul><li>接收时间:'+r.get('stime')+'</li><a>来源:'+ r.get('cname')
                         +"("+r.get('code')+")"
-                        +'</li><li>发震时刻:'
-                        +r.get('time')+'</li>' +
-                        '<li>经纬度:'+ r.get('lon').toFixed(3)+','+ r.get('lat').toFixed(3)+'&nbsp;&nbsp;深度:'
-                        + r.get('depth').toFixed(0)+'km</li>' +
-                        '<li>震级:M'+ (r.get('M')==null?"无":r.get('M').toFixed(1))+', Ml'
+                        +'</a><br><a>发震时刻:'
+                        +r.get('time')+'</a>' +
+                        '<br><a>经纬度:'+ r.get('lon').toFixed(3)+','+ r.get('lat').toFixed(3)+'&nbsp;&nbsp;深度:'
+                        + r.get('depth').toFixed(0)+'km</a>' +
+                        '<br><a>震级:M'+ (r.get('M')==null?"无":r.get('M').toFixed(1))+', Ml'
                         +(r.get('Ml')==null?"无":r.get('Ml').toFixed(1))+', Ms '+ (r.get('Ms')==null?"无":r.get('Ms').toFixed(1))+
-                        '</li>'+
-                        '<li>地名:'+ r.get('location')+'</li></ul>';
-                    console.log(str);
+                        '</a>'+
+                        '<br><a>地名:'+ r.get('location')+'</a></ul>';
+                    //console.log(str);
                     return str;
                 }}
             ],
